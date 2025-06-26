ff22111a9a745c7c20a6e6f02d904435f40d1e9e
LukaOniani <lukaa.oniani2002@gmail.com>
2025-06-26 16:54:30 +0400

    fix exception with EntityGraph

--- src/main/java/com/example/lms/course/repository/CourseRepository.java
+++ src/main/java/com/example/lms/course/repository/CourseRepository.java
@@ -4,22 +4,18 @@ import com.example.lms.course.model.Course;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, UUID> {
 
+  @EntityGraph(attributePaths = {"students", "settings"})
   @Query("SELECT c FROM Course c JOIN c.settings s " +
       "WHERE s.startDate BETWEEN :start AND :end")
   List<Course> findByStartDateBetween(
       @Param("start") LocalDateTime start,
       @Param("end") LocalDateTime end);
 
-  @Query(" select s.email from Course c " +
-      " join c.students s " +
-      " where c.settings.startDate between :start and :end")
-  List<String> findStudentEmailsForCoursesStartingBetween(
-      @Param("start") LocalDateTime start,
-      @Param("end") LocalDateTime end);
 }
