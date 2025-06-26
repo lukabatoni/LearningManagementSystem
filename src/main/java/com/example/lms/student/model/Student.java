b30e3f5ce5c963515a04e390a394ac7de4e26b4a
LukaOniani <lukaa.oniani2002@gmail.com>
2025-06-26 13:01:15 +0400

    2. Add Locale to the Student entity.

--- src/main/java/com/example/lms/student/model/Student.java
+++ src/main/java/com/example/lms/student/model/Student.java
@@ -2,15 +2,22 @@ package com.example.lms.student.model;
 
 import com.example.lms.base.model.BaseEntity;
 import com.example.lms.course.model.Course;
+import com.example.lms.enums.LocaleCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "students")
 public class Student extends BaseEntity {
@@ -30,54 +37,11 @@ public class Student extends BaseEntity {
   @Column(name = "coins")
   private BigDecimal coins;
 
+  @Enumerated(EnumType.STRING)
+  @Column(name = "locale")
+  private LocaleCode locale;
+
   @ManyToMany(mappedBy = "students")
   private Set<Course> courses = new HashSet<>();
 
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public BigDecimal getCoins() {
-    return coins;
-  }
-
-  public void setCoins(BigDecimal coins) {
-    this.coins = coins;
-  }
-
-  public LocalDate getDateOfBirth() {
-    return dateOfBirth;
-  }
-
-  public void setDateOfBirth(LocalDate dateOfBirth) {
-    this.dateOfBirth = dateOfBirth;
-  }
-
-  public Set<Course> getCourses() {
-    return courses;
-  }
-
-  public void setCourses(Set<Course> courses) {
-    this.courses = courses;
-  }
 }
