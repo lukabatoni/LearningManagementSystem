a30a927ac051c48275b83414f89a5f9553df8c4e
LukaOniani <lukaa.oniani2002@gmail.com>
2025-06-27 23:36:42 +0400

    get context from SecurityContextHolder

--- src/main/java/com/example/lms/config/AuditorAwareImpl.java
+++ src/main/java/com/example/lms/config/AuditorAwareImpl.java
@@ -15,7 +15,7 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
     if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
-      return Optional.of("system");
+      return Optional.of("from_system");
     }
 
     return Optional.of(authentication.getName());
